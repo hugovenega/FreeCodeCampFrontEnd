2ed3a1fb4b776c64edfe3012f94fdf97d78e6149
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-25 14:03:22 -0300

    Create app function for return of markdown input textarea

--- markdownPreview/my-app/src/App.js
+++ markdownPreview/my-app/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
+import marked from 'marked';
+
+marked.setOptions({
+  breaks: true
+});
+
+const renderer = new marked.Renderer ();
 
 function App() {
+  const [text, setText] = React.useState('');
   return (
-    
+    <div className='text-center px-4'>
+      <h1 className='p-4'>markdown preview</h1>
+    <textarea
+    name='text'
+    id='text'
+    rows='10'
+    value={text}
+    className='textarea'
+    onChange={(e) => setText(e.target.value)}
+    ></textarea>
+    <h3 className='mt-3'>Output</h3>
+    <Preview markdown={text}/>
+    </div>
   );
 }
+
 
 export default App;
